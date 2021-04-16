0780a9e7537a5fcbaab466b97c33c465448abaea
Aygun <agalarova.aygunn@gmail.com>
2021-04-16 17:36:36 +0400

    14/04/21/homework

---
+++ Aygun-14-04-21/script.js
@@ -0,0 +1,11 @@
+var myimage = document.querySelector("#image");
+function change1() {
+  myimage.src = "img/card1.png";
+}
+function change2() {
+    myimage.src = "img/card-2.webp";
+  }
+  function change3() {
+    myimage.src = "img/card-3.png";
+  }
+
